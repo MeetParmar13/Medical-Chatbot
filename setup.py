7b6f723c7292d305a18e60b437972cc1fb909a27
meet_parmar <meetparmar1394@gmail.com>
2025-03-28 15:05:08 +0530

    folder added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup 
+
+setup(
+    name='Generative-AI Project',
+    version='0.0.0',
+    author='Meet',
+    author_email='meetparmmar1394@gmai.com',
+    packages=find_packages(),
+    requires= []
+)
